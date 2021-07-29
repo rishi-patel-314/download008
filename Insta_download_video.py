9db180fb4201983819192697a760177e463f87c6
Rishi Patel <85552065+rishi-patel-314@users.noreply.github.com>
2021-07-29 12:50:07 +0530

    Add files via upload

---
+++ Insta_download_video.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from tqdm import tqdm
+import requests
+import re
+import sys
+
+
+header={
+    "User-Agent":"Mozilla/5.0(Window NT 6.3;Win64 ; x64) AppleWebkit/537."
+
+}
+
+Insta_url=input("enter the url")
+
+Tail= "?__a=1"
+
+url=Insta_url+Tail
+print (url)
+
+reponse= requests.get(url,headers=header).json()
+print (reponse)
+
+video_location =reponse["graphql"]["shortcode_media"]['video_url']
+print(video_location)
+#hd_video_response=requests.get(video_location,stream=True)
+#print(hd_video_response)
+file_size_request = requests.get(video_location, stream=True)
+file_size = int(file_size_request.headers['Content-Length'])
+block_size = 1024
+filename = datetime.strftime (datetime.now (), '%Y-%m-%d-%H-%M-%S')
+t = tqdm (total=file_size, unit='B', unit_scale=True, desc=filename, ascii=True)
+with open (filename + '.mp4', 'wb') as f:
+    for data in file_size_request.iter_content (block_size):
+        t.update (len (data))
+        f.write (data)
+t.close ()
+print ("Video downloaded successfully")
+
